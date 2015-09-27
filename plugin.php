88e3fb05f4139a0cc7bebf499b87759f9759ba73
tas2580 <tobi.schaefer@gmail.com>
2015-09-27 11:29:05 +0200

    Initial release

---
+++ plugin.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: Flasch Index
+Plugin URI: https://tas2580.net/downloads/download-1.html
+Description: Calculate and display the Flesch-Reading-Ease-Score of an article
+Version: 1.0.0
+Author: tas2580
+Author URI: https://tas2580.net
+License: GPLv2
+*/
+
+
+add_filter('the_content', 'tas2580_flasch_index');
+
+/**
+ * Display the Flesch-Reading-Ease-Score to the article
+ * 
+ * @param	string	$content	The page content
+ */
+if(!function_exists('tas2580_flasch_index'))
+{
+	function tas2580_flasch_index($content)
+	{
+		if(is_single())
+		{
+			$content = $content . '<p>Flesch-Reading-Ease-Score: <b>' . analyze_flasch($content) . '</b></p>';
+		}
+		return $content;
+	}
+}
+
+/**
+ * ACalculates the Flasch Index
+ * 
+ * @param	string	$text	The text for which the Flasch Index is to be calculated
+ */
+if(!function_exists('analyze_flasch'))
+{
+	function analyze_flasch($text)
+	{
+		$num_words = $syllables = 0;
+		$text = strip_tags($text);
+		preg_match_all('/[.!?](\s|$)/', $text, $matches);
+		$sentences = count($matches[0]);
+		if ($sentences == 0)
+		{
+			$sentences = 1;
+		}
+
+		$words = str_word_count($text, 1, 'äüöß');
+		foreach ($words as $word)
+		{
+			$s = 0;
+			$scratch = preg_replace("/e$/", "", strtolower($word));
+			$fragments = preg_split("/[^aeiouy]+/", $scratch);
+			if (!$fragments[0])
+			{
+				array_shift($fragments);
+			}
+			if($fragments && !$fragments[count($fragments) - 1])
+			{
+				array_pop($fragments);
+			}
+			if(count($fragments))
+			{
+				$s += count($fragments);
+			}
+			else
+			{
+				$s = 1;
+			}
+			$syllables += $s;
+			$num_words++;
+		}
+		return round(206.835 - 1.015 * ($num_words / $sentences) - 84.6 * ($syllables / $num_words));
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== tas2580 Flasch Index ===
+Contributors: tas2580
+Donate link: https://tas2580.net/downloads/download-1.html
+Tags: content, flasch
+Requires at least: 3.0.1
+Tested up to: 4.3.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Calculate and display the Flesch-Reading-Ease-Score of an article.
+
+== Description ==
+
+This Plugin calculates the Flasch Index for the content of an article and displays it at the bottom of the article.
+
+For more information about the Flasch Index see: https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests
+
+
+== Installation ==
+
+1. Upload `flasch-index` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0.0 =
+Release Date: September 27, 2015
+* Initial release
